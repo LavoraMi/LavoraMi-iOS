0fe926bf967af085b44dbe42b92a581a645d184a
Andrea Filice <filiceandrea8@gmail.com>
2026-03-17 18:07:39 +0100

    feat: added new splascreen
    * added a clean splashscreen

--- LavoraMi/SplashScreenView.swift
+++ LavoraMi/SplashScreenView.swift
@@ -10,29 +10,32 @@ import SwiftUI
 struct SplashScreenView: View {
     @State private var isActive = false
     @State private var opacity = 1.0
+    @State private var scaleEffect = 1.0
+    @State private var contentOpacity: Double = 0
     
     var body: some View {
-        if(isActive){
+        ZStack {
             ContentView()
-        }
-        
-        else{
-            VStack{
-                VStack{
-                    Image("icon")
-                        .resizable()
-                        .frame(width: 150, height: 150)
-                }
-                .opacity(opacity)
-                .onAppear {
-                    withAnimation(.easeInOut(duration: 0.5)) {
-                        self.opacity = 0.0
-                    }
-                }
+                .opacity(contentOpacity)
+            VStack {
+                Image("icon")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .scaleEffect(scaleEffect)
             }
-            .onAppear{
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5){
-                    self.isActive = true
+            .opacity(opacity)
+            .onAppear {
+                withAnimation(.easeIn(duration: 0.4).delay(0.7)){
+                    scaleEffect = 0.7
+                }
+                withAnimation(.easeIn(duration: 0.4).delay(1)){
+                    scaleEffect = 140
+                }
+                withAnimation(.easeIn(duration: 0.15).delay(1.25)) {
+                    opacity = 0
+                }
+                withAnimation(.easeIn(duration: 0.2).delay(1.15)) {
+                    contentOpacity = 1.0
                 }
             }
         }
