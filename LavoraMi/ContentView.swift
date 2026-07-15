7e9b9b583eac4a7dd0d248f9531b6a7694976f6a
Andrea Filice <filiceandrea8@gmail.com>
2026-07-15 15:50:30 +0200

    fix: fixing colors
    * fixed some colors with Aereoporto interchange

--- LavoraMi/ContentView.swift
+++ LavoraMi/ContentView.swift
@@ -7897,7 +7897,7 @@ func getColor(for line: String) -> Color {
         case "MXP2": return Color(red: 140/255, green: 0, blue: 118/255)
         case "AV": return .red
         case "RV": return .red
-        case "Aereoporto": return .cyan
+        case String(localized: .aereoporto): return .cyan
         case _ where line.contains("R") && !line.contains("RE"): return Color.blue
         case _ where line.contains("RE"): return Color.red
         case let s where (1...33).contains(Int(s) ?? 0): return .orange
