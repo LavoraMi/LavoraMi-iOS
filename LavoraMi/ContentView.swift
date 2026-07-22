d8fc21f0a9f04abb96d8f93419504a3e72565bf8
Andrea Filice <filiceandrea8@gmail.com>
2026-07-22 11:04:32 +0200

    fix: fixing tracks
    * fixed MXP track due to works on that line

--- LavoraMi/ContentView.swift
+++ LavoraMi/ContentView.swift
@@ -4992,7 +4992,7 @@ struct LinesView: View {
     
     var malpensaExpress: [LineInfo] {
         [
-            LineInfo(name: "MXP1", branches: "Malpensa T2 - Milano Centrale", type: "Malpensa Express 1", waitMinutes: "30 min.", stations: StationsDB.mxp1, accessibilityStatus: String(localized: .lineaAccessibile)),
+            LineInfo(name: "MXP1", branches: "Gallarate - Malpensa T2 - Milano Centrale", type: "Malpensa Express 1", waitMinutes: "30 min.", stations: StationsDB.mxp1, accessibilityStatus: String(localized: .lineaAccessibile)),
             LineInfo(name: "MXP2", branches: "Malpensa T2 - Milano Cadorna", type: "Malpensa Express 2", waitMinutes: "30 min.", stations: StationsDB.mxp2, accessibilityStatus: String(localized: .lineaAccessibile)),
         ]
     }
