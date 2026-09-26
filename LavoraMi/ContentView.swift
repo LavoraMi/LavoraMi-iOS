9e3581cbeb862d28a003d324295d817d61f5ebac
Andrea Filice <filiceandrea8@gmail.com>
2026-09-26 13:13:43 +0200

    fix: fixing bugs
    * fixed some bugs with colors

--- LavoraMi/ContentView.swift
+++ LavoraMi/ContentView.swift
@@ -8412,7 +8412,7 @@ func getColor(for line: String) -> Color {
         ///TILO LINES
         case "S10": return Color(red: 226/255, green: 15/255, blue: 26/255)
         case "S20": return Color(red: 0/255, green: 158/255, blue: 224/255)
-        case "S30": return Color(red: 1, green: 144/255, blue: 54/255)
+        case "S30": return Color(red: 1/255, green: 144/255, blue: 54/255)
         case "S40": return Color(red: 120/255, green: 187/255, blue: 114/255)
         case "S50": return Color(red: 128/255, green: 79/255, blue: 16/255)
         case "S90": return Color(red: 254/255, green: 206/255, blue: 0/255)
